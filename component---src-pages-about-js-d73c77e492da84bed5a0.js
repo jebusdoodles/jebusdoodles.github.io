ea85a6d1a9e2f9c1459d7ec8975d5187af7eeeb8
ilhuitem0c <jc11111001001@gmail.com>
2019-12-27 19:57:11 -0600

    Change max width

---
+++ component---src-pages-about-js-d73c77e492da84bed5a0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"3XHS":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),r=n.n(a),c=n("LbRr");t.default=function(){return r.a.createElement("div",{style:{color:"teal"}},r.a.createElement(c.a,{headerText:"Acerca de"}),r.a.createElement("p",null,"Educación"))}},LbRr:function(e,t,n){"use strict";var a=n("q1tI"),r=n.n(a),c={textAlign:"center",fontSize:"32px"};t.a=function(e){return r.a.createElement("h1",{style:c},e.headerText)}}}]);
+//# sourceMappingURL=component---src-pages-about-js-d73c77e492da84bed5a0.js.map
---
+++ styles-5d83b037bd75caeb47f4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"43g+":function(n,w,o){}}]);
+//# sourceMappingURL=styles-5d83b037bd75caeb47f4.js.map
